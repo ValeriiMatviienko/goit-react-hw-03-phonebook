dfbcb16bb3a1154dee8b9a008b70c3e622ebb4e1
ValeriiMatviienko <unanimous276@gmail.com>
2021-03-07 21:46:03 +0200

    add mount

---
+++ src/Filter/Filter.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import s from './Filter.module.css';
+
+const Filter = ({ value, onChange }) => (
+  <div className={s.wrapper}>
+    <label className={s.label}>
+      Find by Name
+      <input
+        className={s.input}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  </div>
+);
+
+Filter.defaultProps = {
+  value: '',
+};
+
+Filter.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Filter;
